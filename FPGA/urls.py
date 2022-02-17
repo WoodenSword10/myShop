bddb453b85df4fedca3d60163a94a78044cb5ad6
ljw <1205793031@qq.com>
2022-02-17 11:14:05 +0800

    补充FPGA内容

--- FPGA/urls.py
+++ FPGA/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.init_FPGA_view),
+    path('introduce/', views.FPGA_introduce_view),
+    path('expore/', views.FPGA_expore_view),
+    path('yj/', views.FPGA_yj_view),
 ]
--- FPGA/views.py
+++ FPGA/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def init_FPGA_view(request):
     return render(request, 'html/FPGA/FPGA_init.html')
+
+def FPGA_introduce_view(request):
+    return render(request, 'html/FPGA/FPGA介绍.html')
+
+def FPGA_expore_view(request):
+    return render(request, 'html/FPGA/FPGA开发流程.html')
+
+def FPGA_yj_view(request):
+    return render(request, 'html/FPGA/硬件描述语言.html')
