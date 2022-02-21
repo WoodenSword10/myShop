ca3c8ee65bb8f949659cd8aded0189ebe71f23c6
ljw <1205793031@qq.com>
2022-02-21 12:05:39 +0800

    补充FPGA内容

--- FPGA/urls.py
+++ FPGA/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('introduce/', views.FPGA_introduce_view),
     path('expore/', views.FPGA_expore_view),
     path('yj/', views.FPGA_yj_view),
+    path('zh/', views.FPGA_ZH_view),
+    path('mbjg/', views.FPGA_MBJG_view),
+    path('xhlx/', views.FPGA_XHLX_view),
 ]
--- FPGA/views.py
+++ FPGA/views.py
@@ -12,3 +12,12 @@ def FPGA_expore_view(request):
 
 def FPGA_yj_view(request):
     return render(request, 'html/FPGA/硬件描述语言.html')
+
+def FPGA_ZH_view(request):
+    return render(request, 'html/FPGA/综合和仿真.html')
+
+def FPGA_MBJG_view(request):
+    return render(request, 'html/FPGA/模板结构.html')
+
+def FPGA_XHLX_view(request):
+    return render(request, 'html/FPGA/信号类型.html')
